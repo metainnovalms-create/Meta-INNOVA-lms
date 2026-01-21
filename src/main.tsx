943b44af6980073311001886fb45b65b424f351f
metainnovalms-create <meta.innova.lms@gmail.com>
2026-01-21 13:31:40 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,7 @@ import "./index.css";
 
 // Polyfill for libraries (e.g. @react-pdf/renderer) that expect Node's Buffer in the browser.
 import { Buffer } from "buffer";
-
-if (!(globalThis as unknown as { Buffer?: typeof Buffer }).Buffer) {
-  (globalThis as unknown as { Buffer: typeof Buffer }).Buffer = Buffer;
-}
+window.Buffer = Buffer;
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
