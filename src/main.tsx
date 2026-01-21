8cf230ea9a7e9522d727411af6b0827ed139cbe8
metainnovalms-create <meta.innova.lms@gmail.com>
2026-01-21 12:23:45 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Polyfill for libraries (e.g. @react-pdf/renderer) that expect Node's Buffer in the browser.
+import { Buffer } from "buffer";
+
+if (!(globalThis as unknown as { Buffer?: typeof Buffer }).Buffer) {
+  (globalThis as unknown as { Buffer: typeof Buffer }).Buffer = Buffer;
+}
+
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
